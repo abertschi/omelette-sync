2bebc93b672b180d77d66347ed8d11c302a2e094
Andrin Bertschi <andrin.bertschi@gmail.com>
2016-03-07 01:04:38 +0100

    introduce post processing to update database after upload/ download

---
+++ src/cloud/google-drive-index.js
@@ -0,0 +1,25 @@
+export default class GoogleDriveIndex {
+
+
+
+  postUpload(source, targetPath, properties, response) {
+
+  }
+
+  postDownload(request, response) {
+
+  }
+
+  postMove(request, response) {
+
+  }
+
+  postRemove(request, response) {
+
+  }
+
+  postCreateFolder(request, response) {
+
+  }
+
+}
