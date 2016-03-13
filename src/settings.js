ab91c5e9df55a70639c6c138e5782cf006debe8c
Andrin Bertschi <andrin.bertschi@gmail.com>
2016-03-13 01:46:14 +0100

    minor fixes

--- src/settings.js
+++ src/settings.js
@@ -1,18 +1,21 @@
 import db from './db.js';
 import Bacon from 'baconjs';
+let debug = require('debug')('bean:app');
 
 class Settings {
 
   get(key) {
     const SQL = 'SELECT value from SETTINGS where key=?';
     return Bacon.fromNodeCallback(db, 'get', SQL, [key])
-      .flatMap(row => row ? row.value || null: null)
+      .flatMap(row => row && row.value ? row.value : null)
+      //.doAction(value => debug('Get %s=%s', key, value))
       .toPromise();
   }
 
   set(key, value) {
     const INSERT = 'INSERT into SETTINGS (key, value) VALUES(?, ?)';
     const UPDATE = 'UPDATE SETTINGS set value=? where key=?';
+    //debug('Set %s=%s', key, value);
     return Bacon.fromPromise(this.get(key))
       .flatMap(existing => {
         if (existing) {
