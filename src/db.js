f107f054eee781c31bf8498fe00ae973b47be9b6
Andrin Bertschi <andrin.bertschi@gmail.com>
2016-03-13 14:05:19 +0100

    add schema for client index

--- src/db.js
+++ src/db.js
@@ -16,6 +16,7 @@ var db = new sqlite3.Database('./mydb.db');
 if (noSchema) {
   db.serialize(function() {
     db.run("CREATE TABLE DIRECTORY_INDEX (path TEXT, is_dir INT, file_id TEXT, payload JSON)");
+    db.run("CREATE TABLE CLIENT_INDEX (key TEXT, path TEXT, payload JSON)");
     db.run("CREATE TABLE CLOUD_INDEX (provider TEXT, key TEXT, payload JSON)");
     db.run("CREATE TABLE UPLOAD_QUEUE (action TEXT, path TEXT, json JSON, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, active INT DEFAULT 0)");
     db.run("CREATE TABLE DOWNLOAD_QUEUE (action TEXT, path TEXT, json JSON, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, active INT DEFAULT 0)");
