f187fb7189147cf5b18103e30278cf333a264d83
Andrin Bertschi <andrin.bertschi@gmail.com>
2016-03-14 21:50:02 +0100

    remove JSON datatype and use plain TEXT instead

--- src/db.js
+++ src/db.js
@@ -18,10 +18,10 @@ if (noSchema) {
 
   db.serialize(function() {
     db.run("CREATE TABLE DIRECTORY_INDEX (path TEXT, is_dir INT, file_id TEXT, payload JSON)");
-    db.run("CREATE TABLE CLIENT_INDEX (key TEXT, path TEXT, payload JSON)");
-    db.run("CREATE TABLE CLOUD_INDEX (provider TEXT, key TEXT, payload JSON)");
-    db.run("CREATE TABLE UPLOAD_QUEUE (action TEXT, path TEXT, json JSON, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, active INT DEFAULT 0)");
-    db.run("CREATE TABLE DOWNLOAD_QUEUE (action TEXT, path TEXT, json JSON, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, active INT DEFAULT 0)");
+    db.run("CREATE TABLE CLIENT_INDEX (key TEXT, path TEXT, payload TEXT)");
+    db.run("CREATE TABLE CLOUD_INDEX (provider TEXT, key TEXT, payload TEXT)");
+    db.run("CREATE TABLE UPLOAD_QUEUE (action TEXT, path TEXT, json TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, active INT DEFAULT 0)");
+    db.run("CREATE TABLE DOWNLOAD_QUEUE (action TEXT, path TEXT, json TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, active INT DEFAULT 0)");
     db.run("CREATE TABLE SETTINGS (key TEXT, value TEXT)");
   });
 }
