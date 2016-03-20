8e94bb02f2b9204c97b9be71212fead0072a5931
Andrin Bertschi <andrin.bertschi@gmail.com>
2016-03-20 15:53:19 +0100

    use settings instead of storage to persist prefs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Watcher from './watcher.js';
-import Storage from './storage.js';
 import colors from 'colors';
 import readline from 'readline';
 import Q from 'Q';
 import Bacon from 'baconjs';
 import fs from 'fs';
+import Settings from './settings.js';
 
 import SyncManager from './sync-manager.js';
 import getGoogleAuthToken from './cloud/get-google-auth-token.js';
@@ -12,21 +12,28 @@ import GoogleDrive from './cloud/google-drive.js';
 
 let log = require('./debug.js')('');
 
-const WATCH_HOMEDIR = '/Users/abertschi/Dropbox/tmp';
+const WATCH_HOMEDIR = '/Users/abertschi/Dropbox/';
 let watcher;
-let lastrun = Storage.getItem('lastrun');
-let initDone = Storage.getItem('initdone');
+let lastrun;
+let initDone;
 let init = true;
 
-if (lastrun && initDone) {
-  lastrun = new Date(lastrun);
-  init = false;
-  log.info('Continuing work from ' + '%s'.green, lastrun);
-} else {
-  log.info('Initializing application');
-}
-
-getGoogleAuthToken().then(bundle => {
+Settings.unmarshall('lastrun')
+.then(value => lastrun = value)
+.then(() => Settings.unmarshall('initdone'))
+.then(value => initDone = value)
+.then(() => {
+  log.info(initDone, lastrun);
+  if (lastrun && initDone) {
+    lastrun = new Date(lastrun);
+    init = false;
+    log.info('Continuing work from ' + '%s'.green, lastrun);
+  } else {
+    log.info('Initializing application');
+  }
+})
+.then(() => getGoogleAuthToken())
+.then(bundle => {
   log.trace('Got Google Auth Bundle', bundle);
 
   let drive = new GoogleDrive({
@@ -43,19 +50,19 @@ getGoogleAuthToken().then(bundle => {
   watcher = new Watcher({
     directory: WATCH_HOMEDIR,
     since: lastrun,
-    init: false,
-    type: 'fswatch'
+    init: init,
+    type: 'shell'
   });
 
   watcher.watch();
 
   watcher.on('index-created', () => {
-    Storage.setItem('initdone', new Date());
+    Settings.marshall('initdone', new Date());
     log.debug('Initializing is done');
   });
 
   watcher.on('changes-since-done', () => {
-    Storage.setItem('last_offline_changes', new Date());
+    Settings.marshall('last_offline_changes', new Date());
     log.debug('Fetching delta is done');
   });
 
@@ -68,7 +75,7 @@ getGoogleAuthToken().then(bundle => {
 process.on('SIGINT', function() {
   let lastrun = new Date();
   log.info('Setting lastrun to %s', lastrun);
-  Storage.setItem('lastrun', lastrun);
+  Settings.marshall('lastrun', lastrun);
   watcher.unwatch();
   setTimeout(function() {
     process.exit(1);
