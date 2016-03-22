03b382d7cb42bb9baaaa98ffb5c019f610eed3a8
Andrin Bertschi <andrin.bertschi@gmail.com>
2016-03-22 02:27:18 +0100

    remove deprecated files

--- src/index/empty-index.js
+++
@@ -1,8 +0,0 @@
-import Bacon from 'baconjs';
-import db from '../db.js';
-
-const REMOVE = 'DELETE FROM DIRECTORY_INDEX WHERE 1';
-
-export default function emptyIndex() {
-  return Bacon.fromCallback(db, 'run', REMOVE, []);
-}
